e51b949ebe29204f183335cf540267a3d3d7078b
nekoextasis <93204226+nekoname@users.noreply.github.com>
2021-12-16 11:34:31 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
+
     public static void main(String[] args) {
-        int i;
+        start();
+    }
+
+    public static void start(){
         Cine c1 = new Cine("Ocimax",5,5);
-        Boolean lleno=false;
-        while (!lleno){
-        }
-        c1.AsignarAsientos();
+        System.out.println(c1.nombre);
+        System.out.println(c1.numAsientos);
+        c1.llenarSala();
         c1.ImprimirTodoArray();
     }
 }
